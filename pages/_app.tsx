1819dc0819a6252f3e7eeaa070f00faa54e033ce
Solomon347 <solomonobialor5@gmail.com>
2026-01-07 22:22:49 +0100

    Wrap Component with ToastProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
+
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import ToastProvider from '../components/ToastProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <ToastProvider />
+      <Component {...pageProps} />
+    </>
+  );
 }
